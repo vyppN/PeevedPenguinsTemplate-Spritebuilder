e7b956d62497952c3c8deb1c4235f87c7db4d56c
vyppN <vyppN@Chaowarits-MacBook-Pro.local>
2015-03-03 15:36:24 +0700

    Automatically Committed for MakeGamesWithUs

--- PeevedPenguins.spritebuilder/Gameplay.m
+++ PeevedPenguins.spritebuilder/Gameplay.m
@@ -83,6 +83,10 @@
 }
 
 -(void)sealRemoved:(CCNode*)seal{
+    CCParticleSystem *explosion = (CCParticleSystem *)[CCBReader load:@"SealExplosion"];
+    explosion.autoRemoveOnFinish = TRUE;
+    explosion.position = seal.position;
+    [seal.parent addChild:explosion];
     [seal removeFromParent];
 }
 
