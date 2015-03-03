e8f1c298fbec762cce21d71392f1baa90492163d
vyppN <vyppN@Chaowarits-MacBook-Pro.local>
2015-03-03 15:22:25 +0700

    Automatically Committed for MakeGamesWithUs

--- PeevedPenguins.spritebuilder/Gameplay.m
+++ PeevedPenguins.spritebuilder/Gameplay.m
@@ -7,6 +7,7 @@
 //
 
 #import "Gameplay.h"
+#import "CCPhysics+ObjectiveChipmunk.h"
 
 @implementation Gameplay{
     CCPhysicsNode *_physicsNode;
@@ -72,7 +73,17 @@
 }
 
 -(void)ccPhysicsCollisionPostSolve:(CCPhysicsCollisionPair *)pair seal:(CCNode *)nodeA wildcard:(CCNode *)nodeB{
-    CCLOG(@"Something collided a seal!");
+    
+    float energy = [pair totalKineticEnergy];
+    if (energy > 5000.f) {
+        [[_physicsNode space] addPostStepBlock:^{
+            [self sealRemoved:nodeA];
+        }key:nodeA];
+    }
+}
+
+-(void)sealRemoved:(CCNode*)seal{
+    [seal removeFromParent];
 }
 
 -(void)touchEnded:(CCTouch *)touch withEvent:(CCTouchEvent *)event{
